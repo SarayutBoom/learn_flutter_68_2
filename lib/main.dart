46c1eaa88dd8dac4125527074375f66db6aed518
Sarayut.S | Boom Piramid Solutions <sarayut@psolutions.co.th>
2026-06-06 14:13:03 +0700

    Step 3: Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,18 @@ class Home extends StatelessWidget {
   const Home({super.key});
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World 2");
+    return Center(
+      child: Container( 
+        color: Colors.blue,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(50),
+        child: Text("Hello World",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),)
+        ),
+    );
   }
 }
