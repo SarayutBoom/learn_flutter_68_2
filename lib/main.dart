bfec6c6a3957c2dcccb840343bfc0c3fa012ad07
Sarayut.S | Boom Piramid Solutions <sarayut@psolutions.co.th>
2026-06-06 14:25:04 +0700

    Step 4: Sperate the home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'screen/home.dart';
+import 'package:learn_flutter_68_2/screen/home.dart';
 
 void main() {
   // runApp(const MyApp());
@@ -17,24 +19,4 @@ void main() {
       ),
     ),
   );
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container( 
-        color: Colors.blue,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: EdgeInsets.all(50),
-        child: Text("Hello World",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),)
-        ),
-    );
-  }
 }
---
+++ lib/screen/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container( 
+        color: Colors.blue,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(50),
+        child: Text("Hello World 5",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),)
+        ),
+    );
+  }
+}
