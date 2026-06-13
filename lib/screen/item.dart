a3c7105f17ea9516a6cded649a5004716214568a
Sarayut.S | Boom Piramid Solutions <sarayut@psolutions.co.th>
2026-06-13 14:04:58 +0700

    Step 10: Make a model for strore data

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:learn_flutter_68_2/model/person.dart';
 class Item extends StatefulWidget {
   const Item({super.key});
 
@@ -8,12 +8,10 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  List data = ["สมชาย", "สมศรี", "สมปอง", "สมหมาย","สมจิตร"];
-
   @override
   Widget build(BuildContext context) { 
     return ListView.builder(
-      itemCount: data.length,
+      itemCount: people.length,
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
@@ -22,10 +20,20 @@ class _ItemState extends State<Item> {
           ),
           margin: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
           padding: EdgeInsets.all(40),
-          child: Text(
-            data[index],
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          )
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("ชื่อ: ${people[index].name}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  Text("อายุ: ${people[index].age} ปี", style: TextStyle(fontSize: 16)),
+                  Text("อาชีพ: ${people[index].job}", style: TextStyle(fontSize: 16)),
+                ],
+              ),
+              Icon(Icons.person, size: 50, color: Colors.green),
+            ],
+          ),
         );
       },
     );
