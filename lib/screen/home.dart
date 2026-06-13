fbfbe9489eef6593c93d925523414105763772e5
Sarayut.S | Boom Piramid Solutions <sarayut@psolutions.co.th>
2026-06-13 10:12:09 +0700

    Step 5 row, coloum, main , axis, cross axis

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,18 +5,55 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container( 
-        color: Colors.blue,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: EdgeInsets.all(50),
-        child: Text("Hello World 5",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),)
-        ),
+    // return Row(
+    return Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.end,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.redAccent,
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            child: Text(
+              "Hello World 5",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+              ),
+            ),
+          ),
+          Container(
+            color: const Color.fromARGB(255, 82, 232, 255),
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            child: Text(
+              "Hello World 5",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+              ),
+            ),
+          ),
+          Container(
+            color: const Color.fromARGB(255, 82, 255, 91),
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            child: Text(
+              "Hello World 5",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+              ),
+            ),
+          ),
+        ],
     );
   }
 }
